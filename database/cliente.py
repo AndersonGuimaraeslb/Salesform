dedd9263dc499383f1a444f21f8f8be68576e4da
AndersonGuimaraeslb <166404583+AndersonGuimaraeslb@users.noreply.github.com>
2024-04-08 10:25:25 -0300

    Add files via upload

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTE= [
+    
+    {"id":1,"nome": "Anderson", "e-mail": "andfson@ags.com"},
+    {"id":2,"nome": "Carlos", "e-mail": "andeadafrson@ags.com"},
+    {"id":3,"nome": "Prdro", "e-mail": "af@ags.com"},
+    {"id":4,"nome": "Judas", "e-mail": "andaffaerson@ags.com"},
+    {"id":5,"nome": "Jose", "e-mail": "andearson@ags.com"},
+]
